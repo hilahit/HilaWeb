881e487bf488b0398becca92e0af867ac0674c65
ManchiGitHub <66803951+ManchiGitHub@users.noreply.github.com>
2021-07-22 21:55:21 +0300

    removed app playground

--- App/app/src/main/java/com/hit/hilaapp/ContactListFragment.java
+++
@@ -1,7 +0,0 @@
-package com.hit.hilaapp;
-
-public class ContactListFragment {
-
-    public final static String CONTACT_NAME = "contact.name";
-    public static final String CONTACT_ID = "contact.id";
-}
